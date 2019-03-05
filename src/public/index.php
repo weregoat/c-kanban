3743d08267de4e8289b233d4b085631b8d3db893
Ruggero Cucchiani <ruggero.cucchiani@exertisztorm.com>
2019-03-05 15:42:03 +0100

    catch exception on web page
    Catch an eventual exception in the web page and return a 500 header, as
    an alternative to die if an env variable is missing.

--- src/public/index.php
+++ src/public/index.php
@@ -1,34 +1,41 @@
 <?php
+
 use KanbanBoard\Authentication;
 use KanbanBoard\GithubClient;
 use KanbanBoard\Utilities;
 use KanbanBoard\DataObjects\Repository;
 
-require_once __DIR__.'/../../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
-$repositoryNames = explode('|', Utilities::env('GH_REPOSITORIES'));
-$pausingLabels = explode('|', Utilities::env('PAUSING_LABELS', ''));
-/* OAuth App authorisation is okay, but private token works too. YMMV */
-/* https://help.github.com/en/articles/creating-a-personal-access-token-for-the-command-line */
-$token = $token = Utilities::env('GH_TOKEN', '');
-if (empty($token)) {
-    $authentication = new Authentication();
-    $token = $authentication->getToken();
-}
+try {
+    $repositoryNames = explode('|', Utilities::env('GH_REPOSITORIES'));
+    $pausingLabels = explode('|', Utilities::env('PAUSING_LABELS', ''));
+    /* OAuth App authorisation is okay, but private token works too. YMMV */
+    /* https://help.github.com/en/articles/creating-a-personal-access-token-for-the-command-line */
+    $token = $token = Utilities::env('GH_TOKEN', '');
+    if (empty($token)) {
+        $authentication = new Authentication();
+        $token = $authentication->getToken();
+    }
 
-$github = new GithubClient($token, Utilities::env('GH_ACCOUNT'));
-/* I find the original handling of multiple repositories absurd */
-/* So I am going to list the milestones by repository */
-$repositories = array();
-foreach($repositoryNames as $name) {
-    $repository = new Repository($name);
-    $repository->fetchMilestones($github, $pausingLabels);
-    $repositories[] = $repository->toArray();
+    $github = new GithubClient($token, Utilities::env('GH_ACCOUNT'));
+    /* I find the original handling of multiple repositories absurd */
+    /* So I am going to list the milestones by repository */
+    $repositories = array();
+    foreach ($repositoryNames as $name) {
+        $repository = new Repository($name);
+        $repository->fetchMilestones($github, $pausingLabels);
+        $repositories[] = $repository->toArray();
+    }
+    /* https://github.com/bobthecow/mustache.php/wiki */
+    /* https://github.com/bobthecow/mustache.php/wiki/Variable-Resolution */
+} catch (\Exception $exception) {
+    error_log($exception->getMessage());
+    header('HTTP/1.1 500 Internal Server error');
 }
-/* https://github.com/bobthecow/mustache.php/wiki */
-/* https://github.com/bobthecow/mustache.php/wiki/Variable-Resolution */
 
 $m = new Mustache_Engine(array(
-	'loader' => new Mustache_Loader_FilesystemLoader('../views'),
+    'loader' => new Mustache_Loader_FilesystemLoader('../views'),
 ));
 echo $m->render('index', array('repositories' => $repositories));
+
